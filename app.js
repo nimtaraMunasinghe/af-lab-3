4c6cd47bc3939cfacbe20a67cb3528aa28df24b7
Nimtara Munasinghe <73902228+nimtaraMunasinghe@users.noreply.github.com>
2023-03-04 09:42:34 +0530

    Write to file

--- app.js
+++ app.js
@@ -1,1 +1,8 @@
 console.log('Hello World!')
+
+//Write to file
+const fs = require('fs');
+fs.writeFile('file.txt', 'Hello World!', function (err) {
+ if (err) throw err;
+ console.log('File saved!');
+});
