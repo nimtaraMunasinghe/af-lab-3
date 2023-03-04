ab79a8bd8192081efa4f0695a7f9ebe3b1a7532b
Nimtara Munasinghe <73902228+nimtaraMunasinghe@users.noreply.github.com>
2023-03-04 09:47:59 +0530

    fixed error

--- app.js
+++ app.js
@@ -1,5 +1,12 @@
 console.log('Hello World!')
 
+//Read file
+const fs = require('fs');
+fs.readFile('file.txt', 'utf8', function (err, data) {
+ if (err) throw err;
+ console.log(data);
+});
+
 //Write to file
 const fs = require('fs');
 fs.writeFile('file.txt', 'Hello World!', function (err) {
@@ -7,9 +14,3 @@ fs.writeFile('file.txt', 'Hello World!', function (err) {
  console.log('File saved!');
 });
 
-//Read file
-const fs = require('fs');
-fs.readFile('file.txt', 'utf8', function (err, data) {
- if (err) throw err;
- console.log(data);
-});
