97cea00281d633cd9593166e3bb4161d98352924
Nimtara Munasinghe <73902228+nimtaraMunasinghe@users.noreply.github.com>
2023-03-04 09:56:25 +0530

    Async/Await

--- app.js
+++ app.js
@@ -54,3 +54,14 @@ const myPromise = new Promise((resolve, reject) => {
    }).catch((error) => {
     console.log(error);
    });
+
+   //Async/Await
+   async function myFunction() {
+    try {
+    const result = await myPromise;
+    console.log(result);
+    } catch (error) {
+    console.log(error);
+    }
+   }
+   myFunction();
